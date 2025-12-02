13fb1d1724e2ba40be35f17c1fc5b8538594df85
EmmanuelMose <emmanuelmose806@gmail.com>
2025-12-02 05:42:08 +0300

    navbar import

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -2,10 +2,13 @@ import Hero from '../components/hero/Hero.tsx';
 import About from '../components/about/About.tsx';
 import CTA from '../components/cta/CTA.tsx';
 import Footer from '../components/footer/Footer.tsx';
+import Navbar from "../components/navbar/Navbar.tsx"
+
 
 const LandingPage = () => {
   return (
     <div>
+      <Navbar />
       <Hero />
       <About />
       <CTA />
